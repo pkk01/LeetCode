2c1f0e1ff7abca2826482deb41f153f8f7902c94
Pratham Kumar <prk61dlw@gmail.com>
2024-09-29 12:18:47 +0530

    initial commit

---
+++ ValidParantheses .java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class ValidParantheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        // if left parantheses --> push
+        // if right parantheses --> pop
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }
+            else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            }
+            else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }
+            else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            }
+            else
+             return false;
+        }
+
+        return stack.isEmpty();
+
+    }
+}
