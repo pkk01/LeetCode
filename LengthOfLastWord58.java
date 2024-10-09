ad82641a10aa64fe8ef73ce6072cf875259cd5ba
Pratham Kumar <prk61dlw@gmail.com>
2024-10-09 22:28:17 +0530

    added problem 58

---
+++ LengthOfLastWord58.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        // remove extra space
+        String newString = s.trim();
+        int stringLength = newString.length();
+
+        // counter to cont the length of the last word
+        int counter = 0;
+
+        // reverse loop until we find a space
+        for (int i = stringLength - 1; i >= 0; i--) {
+
+            if (newString.charAt(i)  != ' '){
+                counter++;
+            }
+            else {
+                break;
+            }
+        }
+        // return counter value
+        return counter;
+    }
+}
